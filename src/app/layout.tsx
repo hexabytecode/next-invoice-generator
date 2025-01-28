347495aed387382b6afe5676fb7bf81b7cf47d50
Aditya Uphade <hexabytecode@gmail.com>
2025-01-28 11:01:52 +0530

    feature: setting up the repository

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Fraunces } from "next/font/google";
 import "./globals.css";
+
+const frauncesSans = Fraunces({
+  subsets: ["latin"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${frauncesSans.className} antialiased`}>
         {children}
       </body>
     </html>
