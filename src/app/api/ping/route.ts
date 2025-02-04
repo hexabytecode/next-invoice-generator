5061693bd5c7e284955ae4a88bd6d8b8e565a87c
Aditya Uphade <hexabytecode@gmail.com>
2025-02-04 15:06:24 +0530

    bug: resolved bundling issue; dumped turbopack for webpack

--- src/app/api/ping/route.ts
+++ src/app/api/ping/route.ts
@@ -1,5 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// Use endpoint as '<PROTOCOL>/<URL>/api/ping?abc=bhaya'
+
 export async function GET(req: NextRequest) {
-  return NextResponse.json({ message: "Pong!" });
+  console.log(req.nextUrl.searchParams);
+  return NextResponse.json({
+    message: "Pong!",
+    query: req.nextUrl.searchParams.get("abc"),
+  });
 }
