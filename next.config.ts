d0fd3cc5c23d765629ae9d9227d2b2b0cc61fad6
Aditya Uphade <hexabytecode@gmail.com>
2025-02-04 14:48:12 +0530

    bug: fixed a build error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // experimental: {
+  //   serverActions: true, // If using server actions
+  // },
 };
 
 export default nextConfig;
--- src/app/api/ping/route.ts
+++ src/app/api/ping/route.ts
@@ -1,9 +1,5 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> {
-  const data = { message: "Hello from serverside function" };
-  res.status(200).json(data);
+export async function GET(req: NextRequest) {
+  return NextResponse.json({ message: "Pong!" });
 }
