e702764fcbeb49323eee618a47e3a135b88b1e8e
Aditya Uphade <hexabytecode@gmail.com>
2025-02-27 12:35:57 +0530

    bug: fixed image fetching issue without auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,5 +6,5 @@ export default function middleware(req: NextApiRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!$|api/ping|api/public-route).*)"], // Allow root and specific public APIs - like ping
+  matcher: ["/((?!$|api/ping|api/public-route|_next/image).*)"], // Allow root and specific public APIs - like ping
 };
