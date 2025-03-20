eb3043fd47bffeda9e0380f0c4a7660d490161a7
Aditya Uphade <hexabytecode@gmail.com>
2025-03-20 19:43:42 +0530

    feature: used persist from zustland middleware to store data in localStorage

--- src/store/invoiceStore.ts
+++ src/store/invoiceStore.ts
@@ -1,16 +1,20 @@
 import { InvoiceStore } from "@/types/invoiceTypes";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useStore = create<InvoiceStore>((set) => ({
-  step: 1,
-  setStep: (step) => set({ step }),
-  invoice: JSON.parse(localStorage.getItem("invoiceFormData") ?? "{}"),
-  setInvoice: (invoice) => {
-    set((state) => ({ invoice: { ...state.invoice, ...invoice } }));
-    localStorage.setItem("invoiceFormData", JSON.stringify(invoice));
-  },
-  clearInvoice: () => {
-    set({ invoice: {} });
-    localStorage.removeItem("invoiceFormData");
-  },
-}));
+export const useStore = create<InvoiceStore>()(
+  persist(
+    (set) => ({
+      step: 0,
+      setStep: (step) => set({ step }),
+      invoice: {},
+      setInvoice: (invoice) => {
+        set((state) => ({ invoice: { ...state.invoice, ...invoice } }));
+      },
+      clearInvoice: () => {
+        set({ invoice: {} });
+      },
+    }),
+    { name: "invoiceStore" }
+  )
+);
