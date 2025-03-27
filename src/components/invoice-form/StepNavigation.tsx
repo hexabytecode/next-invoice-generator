7d41c40e769cbc9d61944ea0abea853ebbaf1aac
Aditya Uphade <hexabytecode@gmail.com>
2025-03-27 12:55:24 +0530

    feature: improved UX by adding css & logos in the buttons

--- src/components/invoice-form/StepNavigation.tsx
+++ src/components/invoice-form/StepNavigation.tsx
@@ -1,7 +1,7 @@
 import { Button } from "../ui/button";
 import { useStore } from "@/store/invoiceStore";
 import { stepsLength } from "@/constants/steps";
-
+import { ArrowLeft, ArrowRight } from "lucide-react";
 interface StepNavigationButtonsProps {
   handleBack?: () => void;
 }
@@ -12,12 +12,19 @@ export const StepNavigationButtons = ({
   const { step, isNextDisabled, isBackDisabled } = useStore();
 
   return (
-    <div className="flex gap-2">
-      <Button disabled={isBackDisabled} type="button" onClick={handleBack}>
+    <div className="flex justify-end gap-4 mt-6">
+      <Button
+        disabled={isBackDisabled}
+        type="button"
+        onClick={handleBack}
+        variant="outline"
+      >
+        <ArrowLeft />
         Back
       </Button>
-      <Button disabled={isNextDisabled} type="submit">
-        {stepsLength == step ? "Submit" : "Next"}
+      <Button disabled={isNextDisabled} type="submit" className="px-6">
+        {step === stepsLength ? "Submit" : "Next"}
+        <ArrowRight />
       </Button>
     </div>
   );
