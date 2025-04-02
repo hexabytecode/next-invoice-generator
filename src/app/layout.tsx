88b14f616d4d61120e2bbcaaa0738bc76e3d0afa
Aditya Uphade <hexabytecode@gmail.com>
2025-04-02 16:10:30 +0530

    feature: finished setup for reactQuery - added metadata in constants dir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { appMetadata } from "@/constants/metadata";
+import { QueryProvider } from "@/components/QueryProvider";
 
-export const metadata: Metadata = {
-  title: "VyaparBill - Simplify Your Billing",
-  description:
-    "VyaparBill helps businesses manage invoices, track expenses, and streamline their billing process with ease.",
-  keywords: ["billing", "invoicing", "business management", "VyaparBill"],
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
+export const metadata: Metadata = appMetadata;
 
 export default function RootLayout({
   children,
@@ -21,15 +15,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="h-screen w-screen flex flex-col">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Header />
-          <main className="flex-1 flex">{children}</main>
-        </ThemeProvider>
+        <QueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="flex-1 flex">{children}</main>
+          </ThemeProvider>
+        </QueryProvider>
       </body>
     </html>
   );
---
+++ src/constants/metadata.ts
@@ -0,0 +1,9 @@
+export const appMetadata = {
+  title: "VyaparBill - Simplify Your Billing",
+  description:
+    "VyaparBill helps businesses manage invoices, track expenses, and streamline their billing process with ease.",
+  keywords: ["billing", "invoicing", "business management", "VyaparBill"],
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
